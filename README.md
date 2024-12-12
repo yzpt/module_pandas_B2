6235e87ba96e101abe9524eecb17580f2e88387d
Yohann Zapart <yzpt.de@gmail.com>
2024-12-12 11:04:05 +0100

    update 7.0 .ipynb & html

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 * [7.0.EDA_template.html](7.0.EDA_template.html)
 * [7.0.EDA_template.ipynb](7.0.EDA_template.ipynb)
+
   
 * [8.0.html_conversion.html](8.0.html_conversion.html)
 * [8.0.html_conversion.ipynb](8.0.html_conversion.ipynb)
--- journal.sh
+++ journal.sh
@@ -46,8 +46,6 @@ jupyter nbconvert --to html 2.0.gapminder_analysis.ipynb && mv 2.0.gapminder_ana
 jupyter nbconvert --to html 4.0.outliers.ipynb && mv 4.0.outliers.html html/
 jupyter nbconvert --to html 7.0.EDA_template.ipynb && mv 7.0.EDA_template.html .
 
-jupyter nbconvert --to html 7.0.EDA_template.ipynb  && git add . && git commit -m "update 7.0.EDA_template.html" && git push
-jupyter nbconvert --to html 8.0.html_conversion.ipynb  && git add . && git commit -m "update 8.0.html_conversion.html" && git push
 
 
 gsutil ls gs://yohann-zapart/
@@ -56,4 +54,11 @@ gsutil cp data/structure_pop_2021.csv gs://yohann-zapart/
 gsutil acl ch -u AllUsers:R gs://yohann-zapart/structure_pop_2021.csv
 # url : 
 https://storage.googleapis.com/yohann-zapart/structure_pop_2021.csv
+
+
+
+jupyter nbconvert --to html 7.0.EDA_template.ipynb  
+jupyter nbconvert --to html 8.0.html_conversion.ipynb
+git add . && git commit -m "update 7.0. & 8.0 + html conversion" && git push
+
 
